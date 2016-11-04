74e2635bcc3b76c17518f4ed9ea9c239b3c83113
Jonas Scheffner <jonas.scheffner@live.de>
2016-11-04 21:52:58 +0100

    tests use nock to mock http requests (instead of sinon)

--- test/details-test.js
+++ test/details-test.js
@@ -4,15 +4,13 @@
  */
 
 var expect = require('chai').expect;
-var rewire = require('rewire');
-var Client = rewire('../');
-var sinon = require('sinon');
-var sandbox = sinon.sandbox.create();
+var Client = require('../');
+var nock = require('nock');
 
 describe('details', function () {
 
     afterEach(function () {
-        sandbox.restore();
+        nock.cleanAll();
     });
 
     it('should expose function #details', function () {
@@ -21,75 +19,89 @@ describe('details', function () {
     });
 
     it('should make request to get details of person with id 6368845', function (done) {
-        var path = 'https://hdmapp.mi.hdm-stuttgart.de/details/anonymous/person/6368845';
+        var path = '/details/anonymous/person/6368845';
         spyDetailsRequest('person', '6368845', path, done);
     });
 
     it('should make request to get details of person with id 111111', function (done) {
-        var path = 'https://hdmapp.mi.hdm-stuttgart.de/details/anonymous/person/111111';
+        var path = '/details/anonymous/person/111111';
         spyDetailsRequest('person', '111111', path, done);
     });
 
     it('should make request to get details of lecture with id 111111', function (done) {
-        var path = 'https://hdmapp.mi.hdm-stuttgart.de/details/anonymous/lecture/111111';
+        var path = '/details/anonymous/lecture/111111';
         spyDetailsRequest('lecture', '111111', path, done);
     });
 
     it('should make request to get details of event with id 111111', function (done) {
-        var path = 'https://hdmapp.mi.hdm-stuttgart.de/details/anonymous/event/111111';
+        var path = '/details/anonymous/event/111111';
         spyDetailsRequest('event', '111111', path, done);
     });
 
     it('should make request to get details of event with id 111111', function (done) {
-        var path = 'https://hdmapp.mi.hdm-stuttgart.de/details/anonymous/room/123456';
+        var path = '/details/anonymous/room/123456';
         spyDetailsRequest('room', '123456', path, done);
     });
 
     it('should not make request if type is invalid', function (done) {
-        var spy = sandbox.spy(Client.__get__('request'), 'get');
+        var client, scope;
+
+        scope = nock('https://hdmapp.mi.hdm-stuttgart.de')
+            .get(/.*/)
+            .query(true)
+            .reply('200');
+
         client = new Client();
-        client.details('test', '123', function () {
-            called = spy.called;
-            expect(called).to.be.false;
+        client.details('food', '1221', function () {
+            expect(scope.isDone()).to.be.false;
             done();
         })
     });
 
     it('should provide an error method if type is invalid', function (done) {
         var client = new Client();
-        client.details('food', '123', function (err) {
+        client.details('food', '1234', function (err) {
             expect(err.message).to.equal('Type food is invalid.');
             done();
         })
     });
 
-    it('should provide the error if api responds with one', function () {
-        var client;
-        sandbox.stub(Client.__get__('request'), 'get').callsArgWith(1, 'Test Error', null, null);
-        client = new Client();
+    it('should provide the error if api responds with one', function (done) {
+        var client = new Client();
+
+        nock('https://hdmapp.mi.hdm-stuttgart.de')
+            .get('/details/anonymous/person/123')
+            .replyWithError('Test Error');
+
         client.details('person', '123', function (err) {
-            expect(err).to.equal('Test Error');
+            expect(err.message).to.equal('Test Error');
+            done();
         })
     });
 
-    it('should provide response body of api call', function () {
-        var body, client;
-        body = 'Test Body';
-        sandbox.stub(Client.__get__('request'), 'get').callsArgWith(1, null, null, body);
-        client = new Client();
+    it('should provide response body of api call', function (done) {
+        var client = new Client();
+        nock('https://hdmapp.mi.hdm-stuttgart.de')
+            .get('/details/anonymous/person/123')
+            .reply(200, 'Test Response');
+
         client.details('person', '123', function (err, data) {
-            expect(data).to.equal(body);
+            expect(data).to.equal('Test Response');
+            done(err, data);
         })
     })
 });
 
 function spyDetailsRequest(type, id, path ,done) {
-    var called, client, spy;
-    spy = sandbox.spy(Client.__get__('request'), 'get');
+    var client, scope;
+
+    scope = nock('https://hdmapp.mi.hdm-stuttgart.de')
+        .get(path)
+        .reply(200, 'Test');
+
     client = new Client();
     client.details(type, id, function () {
-        called = spy.calledWith(path, sinon.match.typeOf('function'));
-        expect(called).to.be.true;
+        scope.done();
         done();
     })
 }
--- test/search-test.js
+++ test/search-test.js
@@ -2,16 +2,14 @@
  *
  * Created by Jonas on 03.11.2016.
  */
-var rewire = require('rewire');
-var sinon = require('sinon');
-var Client = rewire('../');
+var Client = require('../');
 var expect = require('chai').expect;
-var sandbox = sinon.sandbox.create();
+var nock = require('nock');
 
 describe('search', function () {
 
     afterEach(function () {
-        sandbox.restore();
+        nock.cleanAll();
     });
 
     it('should expose function #search', function () {
@@ -20,64 +18,73 @@ describe('search', function () {
     });
 
     it('should make correct call to search System Engineering lecture', function (done) {
-        var path = 'https://hdmapp.mi.hdm-stuttgart.de/search/anonymous/lectures?q=System%20Engineering';
+        var path = '/search/anonymous/lectures';
         spySearchRequest('lecture', 'System Engineering', path, done);
     });
 
     it('should make correct call to search PIA lecture', function (done) {
-        var path = 'https://hdmapp.mi.hdm-stuttgart.de/search/anonymous/lectures?q=PIA';
+        var path = '/search/anonymous/lectures';
         spySearchRequest('lecture', 'PIA', path, done);
     });
 
     it('should make correct call to search person Thomas Pohl ', function (done) {
-        var path = 'https://hdmapp.mi.hdm-stuttgart.de/search/anonymous/persons?q=Thomas%20Pohl';
+        var path = '/search/anonymous/persons';
         spySearchRequest('person', 'Thomas Pohl', path, done);
     });
 
     it('should make correct call to search all types ', function (done) {
-        var path = 'https://hdmapp.mi.hdm-stuttgart.de/search/anonymous/all?q=test';
+        var path = '/search/anonymous/all';
         spySearchRequest('all', 'test', path, done);
     });
 
     it('should make correct call to search room ', function (done) {
-        var path = 'https://hdmapp.mi.hdm-stuttgart.de/search/anonymous/rooms?q=S106';
+        var path = '/search/anonymous/rooms';
         spySearchRequest('room', 'S106', path, done);
     });
 
     it('should make correct call to search event ', function (done) {
-        var path = 'https://hdmapp.mi.hdm-stuttgart.de/search/anonymous/events?q=Party';
+        var path = '/search/anonymous/events';
         spySearchRequest('event', 'Party', path, done);
     });
 
     it('should call cb with the api response as is', function (done) {
-        var testdata, client;
-        testdata = 'test data';
-        sandbox.stub(Client.__get__('request'), 'get').callsArgWith(1, null, null, testdata);
-        client = new Client();
+        var client = new Client();
+        nock('https://hdmapp.mi.hdm-stuttgart.de')
+            .get('/search/anonymous/persons')
+            .query({ q : 'Pohl' })
+            .reply(200, 'Test Response');
+
         client.search('person', 'Pohl', function (err, data) {
-            expect(data).to.equal(testdata);
+            expect(data).to.equal('Test Response');
             done(err, data);
         })
     });
 
     it('should call cb with the error message if api replies with an error', function (done) {
-        var client;
-        sandbox.stub(Client.__get__('request'), 'get').callsArgWith(1, 'Test Error', null, null);
-        client = new Client();
+        var client = new Client();
+
+        nock('https://hdmapp.mi.hdm-stuttgart.de')
+            .get('/search/anonymous/persons')
+            .query({ q : 'Pohl' })
+            .replyWithError('Test Error');
+
         client.search('person', 'Pohl', function (err) {
-            expect(err).to.equal('Test Error');
+            expect(err.message).to.equal('Test Error');
             done();
         })
     });
 
     it('should not make request if type is invalid', function (done) {
-        var client, spy, called;
+        var client, scope;
 
-        spy = sandbox.spy(Client.__get__('request'), 'get');
+        scope = nock('https://hdmapp.mi.hdm-stuttgart.de')
+            .get(/.*/)
+            .query(true)
+            .reply('200');
+
         client = new Client();
         client.search('food', 'Pohl', function () {
-            called = spy.called;
-            expect(called).to.be.false;
+            expect(scope.isDone()).to.be.false;
             done();
         })
     });
@@ -92,12 +99,16 @@ describe('search', function () {
 });
 
 function spySearchRequest (type, query, path, done) {
-    var spy, client, called;
-    spy    = sandbox.spy(Client.__get__('request'), 'get');
+    var client, scope;
+
+    scope = nock('https://hdmapp.mi.hdm-stuttgart.de')
+        .get(path)
+        .query({q: query})
+        .reply(200, 'Test');
+
     client = new Client();
     client.search(type, query, function () {
-        called = spy.calledWith(path, sinon.match.typeOf('function'));
-        expect(called).to.be.true;
+        scope.done();
         done();
     })
 }
